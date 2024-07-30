757b5897614ebba79b33a42208b738f4c866bec7
IsraelSouza <israel.felipe1991@gmail.com>
2024-07-30 05:40:40 -0300

    Iniciado projeto

---
+++ Fundamentos/Models/Pessoa.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fundamentos.Models
+{
+    public class Pessoa
+    {
+        //Atributos
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        // Métodos / funções
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}\n, e tenho {Idade} anos");
+        }
+    }
+}
---
+++ Fundamentos/Program.cs
@@ -0,0 +1,7 @@
+﻿using Fundamentos.Models;
+
+Pessoa p = new Pessoa();
+
+p.Nome = "Israel";
+p.Idade = 26;
+p.Apresentar();
